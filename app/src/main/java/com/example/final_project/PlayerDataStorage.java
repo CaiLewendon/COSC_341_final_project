4aa9481dcb396d73028714e925a61ef5907d27d7
CaiLewendon <cailewendon@hotmail.com>
2025-04-09 01:30:15 -0700

    added better persistent data

--- app/src/main/java/com/example/final_project/PlayerDataStorage.java
+++ app/src/main/java/com/example/final_project/PlayerDataStorage.java
@@ -89,10 +89,54 @@ public class PlayerDataStorage {
 
     // Returns dummy data if no file exists yet, ie everything is emptoy
     private List<Player> getDummyPlayers() {
+        // Dummy JSON data containing 20 players with reviews, this is just the initial values, the data will persist with any changes made too it, this is just so the fields are populated
+        // !!!I REPEAT, THIS IS JUST INIT VALUES, ALL DATA IS PERSISTENT AND CHANGEABLE!!!
+        String dummyJson = "[" +
+                "{\"name\":\"Michael Johnson\",\"reviews\":[{\"reviewerName\":\"Alice\",\"rating\":5,\"reviewText\":\"Amazing performance!\",\"timestamp\":\"2023-04-01\"},{\"reviewerName\":\"Bob\",\"rating\":4,\"reviewText\":\"Really impressive.\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Christopher Smith\",\"reviews\":[{\"reviewerName\":\"Charlie\",\"rating\":3,\"reviewText\":\"Not the best, but okay.\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"James Williams\",\"reviews\":[{\"reviewerName\":\"Diana\",\"rating\":2,\"reviewText\":\"Could use improvement.\",\"timestamp\":\"2023-04-01\"},{\"reviewerName\":\"Edward\",\"rating\":3,\"reviewText\":\"Average performance.\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Patricia Brown\",\"reviews\":[{\"reviewerName\":\"Fiona\",\"rating\":5,\"reviewText\":\"Outstanding skills!\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Linda Jones\",\"reviews\":[{\"reviewerName\":\"George\",\"rating\":4,\"reviewText\":\"Solid performance.\",\"timestamp\":\"2023-04-01\"},{\"reviewerName\":\"Helen\",\"rating\":4,\"reviewText\":\"Very consistent.\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Barbara Garcia\",\"reviews\":[{\"reviewerName\":\"Ian\",\"rating\":3,\"reviewText\":\"Average.\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Elizabeth Martinez\",\"reviews\":[{\"reviewerName\":\"Jack\",\"rating\":4,\"reviewText\":\"Good player.\",\"timestamp\":\"2023-04-01\"},{\"reviewerName\":\"Karen\",\"rating\":5,\"reviewText\":\"Excellent!\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Jennifer Rodriguez\",\"reviews\":[{\"reviewerName\":\"Leo\",\"rating\":2,\"reviewText\":\"Not too impressive.\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Maria Hernandez\",\"reviews\":[{\"reviewerName\":\"Megan\",\"rating\":4,\"reviewText\":\"Really good.\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Susan Lopez\",\"reviews\":[{\"reviewerName\":\"Nathan\",\"rating\":3,\"reviewText\":\"Could be better.\",\"timestamp\":\"2023-04-01\"},{\"reviewerName\":\"Olivia\",\"rating\":3,\"reviewText\":\"Mediocre.\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Margaret Gonzalez\",\"reviews\":[{\"reviewerName\":\"Paul\",\"rating\":5,\"reviewText\":\"Superb!\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Dorothy Wilson\",\"reviews\":[{\"reviewerName\":\"Quinn\",\"rating\":2,\"reviewText\":\"Needs improvement.\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Lisa Anderson\",\"reviews\":[{\"reviewerName\":\"Rachel\",\"rating\":4,\"reviewText\":\"Very talented.\",\"timestamp\":\"2023-04-01\"},{\"reviewerName\":\"Steve\",\"rating\":4,\"reviewText\":\"Consistent.\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Nancy Thomas\",\"reviews\":[{\"reviewerName\":\"Tracy\",\"rating\":3,\"reviewText\":\"Okay player.\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Karen Taylor\",\"reviews\":[{\"reviewerName\":\"Uma\",\"rating\":5,\"reviewText\":\"Fantastic!\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Betty Moore\",\"reviews\":[{\"reviewerName\":\"Victor\",\"rating\":4,\"reviewText\":\"Great skills!\",\"timestamp\":\"2023-04-01\"},{\"reviewerName\":\"Wendy\",\"rating\":5,\"reviewText\":\"Amazing performance!\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Helen Jackson\",\"reviews\":[{\"reviewerName\":\"Xavier\",\"rating\":3,\"reviewText\":\"Not my favorite.\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Sandra Martin\",\"reviews\":[{\"reviewerName\":\"Yvonne\",\"rating\":5,\"reviewText\":\"Excellent work!\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Donna Lee\",\"reviews\":[{\"reviewerName\":\"Zach\",\"rating\":2,\"reviewText\":\"Could be improved.\",\"timestamp\":\"2023-04-01\"}]}," +
+                "{\"name\":\"Carol Perez\",\"reviews\":[{\"reviewerName\":\"Alfred\",\"rating\":4,\"reviewText\":\"Solid performance overall.\",\"timestamp\":\"2023-04-01\"},{\"reviewerName\":\"Bianca\",\"rating\":4,\"reviewText\":\"Good effort.\",\"timestamp\":\"2023-04-01\"}]}" +
+                "]";
+
         List<Player> players = new ArrayList<>();
-        players.add(new Player("Player One", new ArrayList<Review>()));
-        players.add(new Player("Player Two", new ArrayList<Review>()));
-        players.add(new Player("Player Three", new ArrayList<Review>()));
+        try {
+            JSONArray playersArray = new JSONArray(dummyJson);
+            for (int i = 0; i < playersArray.length(); i++) {
+                JSONObject pObj = playersArray.getJSONObject(i);
+                String name = pObj.getString("name");
+                List<Review> reviews = new ArrayList<>();
+                JSONArray reviewsArray = pObj.optJSONArray("reviews");
+                if (reviewsArray != null) {
+                    for (int j = 0; j < reviewsArray.length(); j++) {
+                        JSONObject rObj = reviewsArray.getJSONObject(j);
+                        String reviewerName = rObj.getString("reviewerName");
+                        int rating = rObj.getInt("rating");
+                        String reviewText = rObj.getString("reviewText");
+                        String timestamp = rObj.getString("timestamp");
+                        reviews.add(new Review(reviewerName, rating, reviewText, timestamp));
+                    }
+                }
+                players.add(new Player(name, reviews));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return players;
     }
 }
